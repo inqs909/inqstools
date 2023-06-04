0cccd9f30162695d44f8f51cb6396c7cac818462
Isaac QS <inqs.909@gmail.com>
2023-06-03 17:16:55 -0700

    Created new CI template and function

---
+++ R/ci_learnr.R
@@ -0,0 +1,28 @@
+#' Title
+#'
+#' A wrapper function that will create a learnr interactive document with the CSUCI theme.
+#'
+#' @param name A string value containing the name of the tutorial you want to create.
+#'
+#' @return Creates a learnr tutorial with important components to create CI learnr tutorial.
+#' @export
+#'
+#' @examples ci_learnr("test")
+#'
+ci_learnr <- function(name) {
+  if (!dir.exists("inst/tutorials/")) {
+    dir.create("inst/tutorials/")
+  }
+  file_name <- paste0("inst/tutorials/", name, ".Rmd")
+  rmarkdown::draft(file_name,
+                   template = "CI_learnr",
+                   package = "inqstools",
+                   edit = FALSE)
+  new_file <- paste0("inst/tutorials/",
+                     name,
+                     "/", name,  ".Rmd")
+  file.edit(new_file)
+  print(paste0("New file created here: ", new_file))
+}
+
+
